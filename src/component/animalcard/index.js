1fde529a09cf8cda5b0f047cb8ceefb01c5c1af8
Tori Fortune <toriolley@gmail.com>
2019-07-11 21:45:53 -0500

    initiated netlify for deploying app

--- src/component/animalcard/index.js
+++ src/component/animalcard/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.css";
-function AnimalCard (props){
-    return(
-        <div className = "card">
-            <div className = "img-container">
-                <img alt = {props.name} src = {props.image}/>
+function AnimalCard(props) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
 
-        </div>
+            </div>
         </div>
     )
 }
