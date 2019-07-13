be382e065227e7b8d6318a7e255c2d1c79b616e2
Tori Fortune <toriolley@gmail.com>
2019-07-12 21:07:42 -0500

    deleted container and navbar, revised card component name. completed render method, game over and card click, updated exported files in App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import AnimalCard from "./component/animalcard/";
-import Navbar from "./component/navbar";
-import fish from "./fishes.json";
+import Card from "./component/Card";
+import Header from "./component/Header";
+import cards from "./cards.json";
 import "./App.css";
 
 
@@ -10,94 +10,212 @@ import "./App.css";
 
 class App extends Component{
   state={
-    fish,
+    cards,
     currentScore: 0,
     topScore: 0
   };
 
-  selectFish = (name) =>{
-    let thisFish= this.state.fishes.filter(fish=>fish.name=== name)[0],
-    currentScore=this.state.currentScore +1;
-    if (thisFish.clicked)
-    {
-      this.gameOver(false, name)
+  class App extends Component {
+    // Setting this.state.cards to the cards json array
+    state = {
+      cards,
+      score: 0,
+      topScore: 0
+    };
+  
+    gameOver = () => {
+      if (this.state.score > this.state.topScore) {
+        this.setState({topScore: this.state.score}, function() {
+          console.log(this.state.topScore);
+        });
+      }
+      this.state.cards.forEach(card => {
+        card.count = 0;
+      });
+      alert(`Game Over :( \nscore: ${this.state.score}`);
+      this.setState({score: 0});
+      return true;
     }
-    else {
-      if (!thisFish.clicked && currentScore===12) return this.gameOver(true);
-      thisFish.clicked=true;
-      this.setState(
-        {
-          currentScore: currentScore,
-          topScore: (currentScore > this.state.topScore) ? currentScore : this.state.topScore,
-          fishes: this.shuffle(this.state.fishes)
+  
+    clickCount = id => {
+      this.state.cards.find((o, i) => {
+        if (o.id === id) {
+          if(cards[i].count === 0){
+            cards[i].count = cards[i].count + 1;
+            this.setState({score : this.state.score + 1}, function(){
+              console.log(this.state.score);
+            });
+            this.state.cards.sort(() => Math.random() - 0.5)
+            return true; 
+          } else {
+            this.gameOver();
+          }
         }
+      });
+    }
+  // Map over this.state.cards and render a fishCard component for each fish object
+    render() {
+      return (
+        <Wrapper>
+          <Header score={this.state.score} topScore={this.state.topScore}>Click Bait</Header>
+          {this.state.cards.map(card => (
+            <Card
+              clickCount={this.clickCount}
+              id={card.id}
+              key={card.id}
+              image={card.image}
+            />
+          ))}
+        </Wrapper>
       );
     }
-  };
-
-gameOver=(win,name)=>{
-  var message = (win)? "Awesome job!": `Try again,${name}, has been picked already;`
-alert (message);
-this.state.fishes.filter(fish=>fish.clicked===true).map(fish=>fish.clicked =false);
-this.setState(
-  {
-    currentScore:0,
-    fishes: this.shuffle(this.state.fish)
   }
-)
-
-}
-
-// to review https://javascript.info/task/shuffle look for shuffle script
-
-
-// render(){
-//   return <div> Hello World</div>
-// }
-
-shuffle = (array) => {
-  let i = 0
-    , j = 0
-    , temp = null
-
-  for (i = array.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-  return array;
-};
+  
+  export default App;
+  
 
 
 
-render() {
-  return (
-    <div>
-      <Navbar 
-        currentScore={this.state.currentScore}
-        topScore={this.state.topScore}
-      />
-      <div className="cardHolder row">
-            {this.state.fishes.map(fish => (
-                <AnimalCard
-                  key={fish.name}
-                  name={fish.name}
-                  image={fish.image}            
-                  clickCard={this.clickCard}
-                />
-            ))} 
-      </div>
-    </div>
-  );
-}
-}
-
-export default App;
 
 
-
+  
+// gameOver=(win,name)=>{
+//   var message = (win)? "Awesome job!": `Try again,${name}, has been picked already;`
+// alert (message);
+// this.state.cards.filter(fish=>fish.clicked===true).map(fish=>fish.clicked =false);
+// this.setState(
+//   {
+//     currentScore:0,
+//     cards: this.shuffle(this.state.fish)
+//   }
+// )
 
 // }
 
-export default App;
+
+// //   gameOver = ()=>{
+// //     if (this.state.currentScore>this.state.topScore){
+// //       this.setState({topScore:this.state.currentScore}, function(){
+
+// //       });
+// //     }
+// //   }
+// // this.state.cards.forEach(card=>{
+// //   card.count=0;
+// // });
+
+
+
+
+
+
+
+
+
+
+
+
+//   selectFish = (name) =>{
+//     let thisFish= this.state.cards.filter(fish=>fish.name=== name)[0],
+//     currentScore=this.state.currentScore +1;
+//     if (thisFish.clicked)
+//     {
+//       this.gameOver(false, name)
+//     }
+//     else {
+//       if (!thisFish.clicked && currentScore===12) return this.gameOver(true);
+//       thisFish.clicked=true;
+//       this.setState(
+//         {
+//           currentScore: currentScore,
+//           topScore: (currentScore > this.state.topScore) ? currentScore : this.state.topScore,
+//           cards: this.shuffle(this.state.cards)
+//         }
+//       );
+//     }
+//   };
+
+// // to review https://javascript.info/task/shuffle look for shuffle script
+
+
+// // render(){
+// //   return <div> Hello World</div>
+// // }
+
+// shuffle = (array) => {
+//   let i = 0
+//     , j = 0
+//     , temp = null
+
+//   for (i = array.length - 1; i > 0; i -= 1) {
+//     j = Math.floor(Math.random() * (i + 1))
+//     temp = array[i]
+//     array[i] = array[j]
+//     array[j] = temp
+//   }
+//   return array;
+// };
+
+
+
+
+
+
+
+// render() {
+//   return (
+//     <Wrapper>
+//       <Header score={this.state.score} highscore={this.state.highscore}>Click Bait</Header>
+//       {this.state.cards.map(card => (
+//         <AnimalCard
+//           clickCount={this.clickCount}
+//           id={fishcards.id}
+//           key={fishcards.id}
+//           image={fishcards.image}
+//         />
+//       ))}
+//     </Wrapper>
+//   );
+// }
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+// // render() {
+// //   return (
+// //     <div>
+// //       <Navbar 
+// //         currentScore={this.state.currentScore}
+// //         topScore={this.state.topScore}
+// //       />
+// //       <div className="cardHolder row">
+// //             {this.state.cards.map(fish => (
+// //                 <AnimalCard
+// //                   key={fish.name}
+// //                   name={fish.name}
+// //                   image={fish.image}            
+// //                   clickCard={this.clickCard}
+// //                 />
+// //             ))} 
+// //       </div>
+// //     </div>
+// //   );
+// // }
+// // }
+
+// // export default App;
+
+
+
+
+// // // }
+
