3080b0db7bcc3f8925110b14d9c7447a3dfe695f
Tori Fortune <toriolley@gmail.com>
2019-07-16 19:23:28 -0500

    deployment troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Wrapper from "./component/Wrapper/Wrapper"
-import Card from "./component/Card/Card"
-import Header from "./component/Header/Header";
+import Wrapper from "./component/wrapper/wrapper"
+import Card from "./component/card/card"
+import Header from "./component/header/Header";
 import cards from "./cards.json";
 import "./App.css";
 
